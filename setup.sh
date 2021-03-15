3e436ec57e6601fd86a1e91544788d6e5ac4a965
Daisey Patrica <dpatrica@va-a2.kzn.21-school.ru>
2021-03-15 22:30:59 +0300

    new

--- setup.sh
+++ setup.sh
@@ -1,10 +1,28 @@
+#!/bin/sh
+
+# eval $(minikube docker-env)
+minikube stop && minikube delete --all
+docker system prune -a -f
+minikube start --driver=docker --extra-config=apiserver.service-node-port-range=1-65535
 eval $(minikube docker-env)
-docker build -t kek_nginx srcs/nginx/.
-echo "Nginx is keked!"
+minikube addons enable metallb
+minikube addons enable metrics-server
+minikube addons enable dashboard
+# eval $(minikube docker-env)
+docker build -t nginx srcs/nginx/.
+echo "kek_nginx is keked!"
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-kubectl apply -f srcs/metallb/metallb.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl apply -f srcs/metallb/metallb.yaml
 kubectl apply -f srcs/nginx/nginx.yaml
-# On first install only
+
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl diff -f - -n kube-system
+
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/strictARP: false/strictARP: true/" | \
+kubectl apply -f - -n kube-system
--- srcs/nginx/service_on.sh
+++ srcs/nginx/service_on.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-/etc/init.d/sshd start
+/etc/init.d/sshd start 2>/dev/null
+/etc/init.d/sshd status
 nginx -g "daemon off;"
